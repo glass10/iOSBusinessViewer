0dec431a5a8f6e706746a574902b0a2cbdcd53f5
Cameron Glass <glass10@purdue.edu>
2017-01-13 23:45:41 -0500

    Added Multiple businesses, force touch, and faster image capturing. Type still not working

---
+++ iDev Business Viewer/Business.swift
@@ -0,0 +1,13 @@
+//
+//  Business.swift
+//  iDev Business Viewer
+//
+//  Created by Cameron Glass on 1/13/17.
+//  Copyright © 2017 Cameron Glass. All rights reserved.
+//
+
+import UIKit
+
+class Business: NSObject {
+
+}
---
+++ iDev Business Viewer/BusinessCell.swift
@@ -0,0 +1,24 @@
+//
+//  BusinessCell.swift
+//  iDev Business Viewer
+//
+//  Created by Cameron Glass on 1/13/17.
+//  Copyright © 2017 Cameron Glass. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
